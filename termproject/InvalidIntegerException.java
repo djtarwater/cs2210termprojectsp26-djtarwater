c60b8c2eba2e42577a7f42b75a6a27515e4fcf38
dtarwater <dtarwater@cedarville.edu>
2026-04-20 14:35:46 -0400

    Setup Process

---
+++ termproject/InvalidIntegerException.java
@@ -0,0 +1,18 @@
+package termproject;
+
+/**
+ * Title:        Project #7
+ * Description:
+ * Copyright:    Copyright (c) 2001
+ * Company:
+ * @author      David Tarwater and Jadon Spears
+ * @version 1.0
+ */
+
+public class InvalidIntegerException extends RuntimeException {
+
+    public InvalidIntegerException(String errorMsg) {
+        super (errorMsg);
+    }
+}
+//Final Test
